3510abbc87bd29b3c9f6e45e4a7f12e8c432b9c7
Varun Raj <varunraj82@gmail.com>
2020-07-30 13:04:55 -0400

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -19,11 +20,32 @@ const items = [
 ]
 
 
+const options = [
+    {
+        label:'The Color Red',
+        value: 'red'
+    },
+    {
+        label:'The Color Green',
+        value:'green'   
+    },
+    {
+        label:'The Shade of Blue',
+        value:'blue'
+    }
+]
+
+
 const App = () =>{
+
+    const[selected, setSelected] = useState(options[0]);
+
     return(
         <div>
-            <Search />
-
+            <Dropdown selected ={selected}
+                      onSelectedChange={setSelected}
+                      options={options}
+            />
         </div>
 
     )
---
+++ src/components/Dropdown.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+
+const Dropdown = ( {options, selected, onSelectedChange } ) => {
+    
+    const [open,setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(()=>{
+
+        const onBodyClick = (event)=>{
+           
+            // get a reference to dropdown created by react and check
+            // if it contains the target clicked by onclick.
+            // If yes, do not set open to false.
+           
+            if (ref.current.contains(event.target)){
+                return
+            } else
+                setOpen(false)
+            } 
+
+
+        document.body.addEventListener('click', onBodyClick);
+        
+        // remove the body event listener if no dropdown is shown in screen.
+        
+        return() => {
+            document.body.removeEventListener('click', onBodyClick)
+        }
+
+    },[])
+
+    const renderedOptions = options.map((option)=>{
+        
+        if (option.value === selected.value) {
+            return null
+        }
+        
+        return(
+            <div key={option.value} 
+                 className="item"
+                 onClick={()=>onSelectedChange(option)}
+                 >
+                {option.label}
+            </div>
+        )
+    })
+    
+    
+    return(
+        <div ref = {ref} className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div onClick={()=>setOpen(!open)} 
+                     className={`ui selection dropdown ${open ? 'visible active':''}`}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition':''}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Dropdown;
